dadb5ad55c8092bfa4bfdf3c7111d63d10924975
MohammadAliHasanLafta <1380lafta@gmail.com>
2024-02-24 05:41:48 -0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <body class="bg-red-400">
+        <h1 class="bg-red-100 text-3xl font-bold underline flex justify-center">
+            Hello world!
+        </h1>        
+    </body>
+</template>
